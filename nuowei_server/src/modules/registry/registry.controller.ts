0804b933b944c52fd106917308ee987d14794661
11477872997 <977139863@qq.com>
2024-05-14 18:08:59 +0800

    feat：更新

--- nuowei_server/src/modules/registry/registry.controller.ts
+++ nuowei_server/src/modules/registry/registry.controller.ts
@@ -1,9 +1,11 @@
 import { Controller,Get} from '@nestjs/common';
+import { ApiTags,ApiOperation } from '@nestjs/swagger';
 import { RegistryService } from './registry.service';
+@ApiTags("基础数据")
 @Controller('registry')
 export class RegistryController {
     constructor(private readonly registryService: RegistryService) {}
-
+    @ApiOperation({ summary: '插入系统默认初始化数据' })
     @Get()
     async setAuth(){
         return this.registryService.setAuth();
