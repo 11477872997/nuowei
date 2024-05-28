0240fa63f0b6aa5120244c6cd8fb94219d706379
11477872997 <977139863@qq.com>
2024-05-28 18:29:50 +0800

    feat:更新用户文档

--- nuowei_server/src/modules/admin/dto/resinfo.ts
+++ nuowei_server/src/modules/admin/dto/resinfo.ts
@@ -1,12 +1,49 @@
 import { ApiPropertyOptional} from '@nestjs/swagger';
+
+export class Theme {
+  @ApiPropertyOptional({ description: '默认背景'})
+  menuBg: string
+  @ApiPropertyOptional({ description: '展开背景'})
+  menuSubBg: string
+  @ApiPropertyOptional({ description: '默认文字'})
+  menuText: string
+  @ApiPropertyOptional({ description: '选中文字'})
+  menuActiveText: string
+  @ApiPropertyOptional({ description: '当前选中展开文字'})
+  menuSubActiveText: string
+  @ApiPropertyOptional({ description: 'hover背景'})
+  menuHoverBg: string
+
+}
+export class User {
+  @ApiPropertyOptional({ description: '名称'})
+  name: string
+  @ApiPropertyOptional({ description: 'id'})
+  id: string
+  @ApiPropertyOptional({ description: '1:管理员,0:普通'})
+  admin: string
+  @ApiPropertyOptional({ description: '1:启用,2:禁用'})
+  status: string
+  @ApiPropertyOptional({ description: '头像'})
+  url: string
+  @ApiPropertyOptional({ description: '用户角色'})
+  rolesId: string
+
+}
+// 登录接口
 export class LoginRes {
-    @ApiPropertyOptional({ description: '用户名'})
-    name: string
-    @ApiPropertyOptional({ description: '用户id'})
-    uid: string
-    @ApiPropertyOptional({ description: '1:管理员,0:普通'})
-    admin: number
     @ApiPropertyOptional({ description: '接口验证'})
     token: string
+    @ApiPropertyOptional({ description: '角色key'})
+    roleKey: Array<any>
+    @ApiPropertyOptional({ description: '角色value'})
+    userRole: string
+    @ApiPropertyOptional({ description: 'true admin用户 false 普通用户'})
+    roleAdmin: boolean
+    @ApiPropertyOptional({ type: Theme })
+    theme: Theme
+    @ApiPropertyOptional({ type: User })
+    user: User
  
   }
+ 
