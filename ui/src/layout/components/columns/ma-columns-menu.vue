3e1a9c821ce3c9867ec78ecd823a1d41bfc50967
11477872997 <977139863@qq.com>
2024-06-17 11:01:16 +0800

    feat:过滤

--- ui/src/layout/components/columns/ma-columns-menu.vue
+++ ui/src/layout/components/columns/ma-columns-menu.vue
@@ -1,13 +1,3 @@
-<!--
- - MineAdmin is committed to providing solutions for quickly building web applications
- - Please view the LICENSE file that was distributed with this source code,
- - For the full copyright and license information.
- - Thank you very much for using MineAdmin.
- -
- - @Author X.Mo<root@imoi.cn>
- - @Link   https://gitee.com/xmo/mineadmin-vue
--->
-
 <template>
   <div class="sider customer-scrollbar flex flex-col items-center bg-gray-800 dark:border-blackgray-5">
     <a-avatar class="mt-2" :size="40"><img :src="`${$url}logo.svg`" class="bg-white" /></a-avatar>
